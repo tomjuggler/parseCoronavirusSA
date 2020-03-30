6974d301a474e7fbf5f5b21cbec7ddcd0d8148ec
tom <tomjuggler@gmail.com>
2020-03-30 12:29:47 +0200

    new data from Pretoria University, now parsed and added to map much more up to date now a bit of a hack but it works
    Also Tested http requests on Android, unfortunately not working.
    
    Signed-off-by: tom <tomjuggler@gmail.com>

--- android/app/src/main/java/parseCoronavirusSA/parseCoronavirusSA.java
+++ android/app/src/main/java/parseCoronavirusSA/parseCoronavirusSA.java
@@ -1,26 +1,44 @@
+package parseCoronavirusSA;
+
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import http.requests.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class parseCoronavirusSA extends PApplet {
+
 ////////////////////new total://///////////////
-import http.requests.*;
+
 JSONArray values;
 JSONObject corona;
 //////////////////////////////////////////////
 Table table;
-String[] provinceNames = {"WC", "KZN", "GP", "MP", "LP", "NW", "FS", "EC", "NC", "UNKNOWN", "total"};
-
-int[] provinces = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int[] lat = {110, 370, 305, 360, 325, 235, 250, 250, 130, 290, 0};
-int[] lon = {355, 210, 130, 125, 65, 145, 225, 315, 240, 370, 0};
+String[] provinceNames = {"WC", "KZN", "GP", "MP", "LP", "NW", "FS", "EC", "NC"};
+int[] provinces = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+int[] lat = {110, 370, 305, 360, 325, 235, 250, 250, 130};
+int[] lon = {355, 210, 140, 125, 65, 145, 225, 315, 240};
 int total = 0;
-int completedTotal = 0;
-boolean ready = false;
+
 PImage map1;
 
-void setup() {
+public void setup() {
   total = 0; //double check we get this right at the start
   noLoop(); //new try this
-  size(800, 800);
+  
   /////////////////////new total://////////////////////////////
     GetRequest get3 = new GetRequest("http://thevirustracker.com/free-api?countryTotal=ZA"); 
-  get3.send(); // program will wait until the request is completed
+  get3.send(); // program will wait untill the request is completed
   corona = parseJSONObject(get3.getContent()); 
     values = corona.getJSONArray("countrydata");
    JSONObject valueObj = values.getJSONObject(0);
@@ -35,26 +53,8 @@ textSize(20);
   //offline for testing:
 //  table = loadTable("data.csv", "header, csv");
   //online:
-  
-  //table = loadTable("https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_confirmed.csv", "header, csv");
-  table = loadTable("https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_provincial_cumulative_timeline_confirmed.csv", "header, csv");
-for (TableRow row : table.rows()) {
-  total++;
-}
-println(total);
+  table = loadTable("https://raw.githubusercontent.com/dsfsi/covid19za/master/data/covid19za_timeline_confirmed.csv", "header, csv");
 
-TableRow row = table.getRow(total-1);
-//println(row);
-
-//while(!ready){
-for(int i = 0; i < provinces.length; i++){
-  int provinceData = row.getInt(provinceNames[i]); //number of cases in eg EC loaded
-    provinces[i] = provinceData; 
-    println(provinces[i]);
-   }
-    
-//}
-/*
   for (TableRow row : table.rows()) {
     total++;
     String num = row.getString("case_id");
@@ -70,11 +70,8 @@ for(int i = 0; i < provinces.length; i++){
     println("province: " + province);
     println("");
   }
-*/
+
 //  println(table.getRowCount() + " total rows in table"); 
-
-
-//using this just need to parse data from new table:
 for(int i = 0; i < provinces.length; i++){
   fill(0);
 //  text("total " + provinceNames[i] + ": " + provinces[i], 20, 30*i+30);
@@ -89,8 +86,6 @@ println("total " + provinceNames[i] + ": " + provinces[i]);
 float latAdj = map(340, 0, 450, 0, width);
 float lonAdj = map(350, 0, 383, 0, height);
 text(newTotal, latAdj, lonAdj); //total or newTotal.. newTotal more up to date
-//unknown
-float latAdj2 = map(290, 0, 450, 0, width);
-float lonAdj2 = map(370, 0, 383, 0, height);
-text("unknown province:", latAdj2-180, lonAdj2);
+}
+  public void settings() {  size(800, 800); }
 }
